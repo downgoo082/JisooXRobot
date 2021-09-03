8254ad14084c9b30a15665dc3b800290c6fb7b62
herlina_junior <89119326+downgoo082@users.noreply.github.com>
2021-09-03 14:34:04 +0800

    Update misc.py

--- JisooX/modules/helper_funcs/misc.py
+++ JisooX/modules/helper_funcs/misc.py
@@ -67,9 +67,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("◀", callback_data="{}_prev({})".format(prefix, modulo_page)),
+            (EqInlineKeyboardButton("◀ Back", callback_data="{}_prev({})".format(prefix, modulo_page)),
                 EqInlineKeyboardButton("Back", callback_data="bot_start"),
-             EqInlineKeyboardButton("▶", callback_data="{}_next({})".format(prefix, modulo_page)))]
+             EqInlineKeyboardButton("Next ▶", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Back", callback_data="bot_start")]]
@@ -80,7 +80,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
 
 def send_to_list(bot: Bot, send_to: list, message: str, markdown=False, html=False) -> None:
     if html and markdown:
-        raise Exception("Can only send with either markdown or HTML!")
+        raise Exception("Hanya dapat mengirim dengan Markdown atau HTML!")
     for user_id in set(send_to):
         try:
             if markdown:
